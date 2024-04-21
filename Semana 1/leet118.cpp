beb6b8266242bc349adad413ba3cdaeae831d20c
Vinicius Castro <viniciuscastro0711@gmail.com>
2024-04-21 11:18:54 -0300

    leet118

--- Semana 1/leet118.cpp
+++ Semana 1/leet118.cpp
@@ -8,12 +8,19 @@ public:
     vector<vector<int>> generate(int numRows) {
         vector<vector<int>> triangle;
         vector<int> row;
-        for (int i = 1; i <= numRows; i++){
-            if (i = 1) row.push_back(1);
-            else if (i = 2) for (int j = 0; j < 2; j++) row.push_back(1);
-            else for (int j = 0; j < i - 1; j++) row.push_back(triangle[i - 1][j] + triangle[i - 1][j+1]);
+        for (int i = 0; i < numRows; i++){
+            if (i == 0) row = {1};
+
+            else if (i == 1) row = {1, 1};
+
+            else for (int j = 0; j <= i; j++) {
+                if (j == 0) row.push_back(1);
+                else if (j == i) row.push_back(1);
+                else row.push_back(triangle[i - 1][j - 1] + triangle[i - 1][j]);
+            }
+
             triangle.push_back(row);
-            while (triangle.size()) triangle.pop_back();
+            while (row.size()) row.pop_back();
         }
         return triangle;
     }
@@ -21,12 +28,12 @@ public:
 
 int main(){
 
-    int numRows;
-    vector<vector<int>> triangle;
+    int n;
+    vector<vector<int>> tri;
     Solution sol;
-    cin >> numRows;
-    triangle = sol.generate(numRows);
-    for (vector v : triangle){
+    cin >> n;
+    tri = sol.generate(n);
+    for (vector v : tri){
         for (int i : v){
             cout << i << " ";
         }
