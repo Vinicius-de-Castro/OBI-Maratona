6230c440e2f65d7fe80c2be8cddf6673eb4d8652
Vinicius Castro <viniciuscastro0711@gmail.com>
2024-04-21 14:18:41 -0300

    bee2411

--- Semana 1/bee2411.cpp
+++ Semana 1/bee2411.cpp
@@ -4,7 +4,50 @@ using namespace std;
  
 int main() {
  
-    
+    int x = 4, y = 3, n, move, steps = 0;
+
+    cin >> n;
+    while (n--){
+        cin >> move;
+        steps++;
+        switch (move){
+            case 1:
+                x += 1;
+                y += 2;
+                break;
+            case 2:
+                x += 2;
+                y += 1;
+                break;
+            case 3:
+                x += 2;
+                y -= 1;
+                break;
+            case 4:
+                x += 1;
+                y -= 2;
+                break;
+            case 5:
+                x -= 1;
+                y -= 2;
+                break;
+            case 6:
+                x -= 2;
+                y -= 1;
+                break;
+            case 7:
+                x -= 2;
+                y += 1;
+                break;
+            case 8:
+                x -= 1;
+                y += 2;
+                break;
+        }
+        if ((x == 1 && y == 3) || (x == 2 && y == 3) || (x == 2 && y == 5) || (x == 5 && y == 4)) break;
+    }
  
+    cout << steps << endl;
+
     return 0;
 }
