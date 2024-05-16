3f8b1ca0c5c7f03b11f35e6ff656a83e6c1a5772
Vinicius Castro <150952242+Vinicius-de-Castro@users.noreply.github.com>
2024-05-16 17:38:39 -0300

    Update m-familia-real.cpp

--- Semana 3/m-familia-real.cpp
+++ Semana 3/m-familia-real.cpp
@@ -5,13 +5,14 @@ int main(){
 
     int n, m;
     cin >> n >> m;
-    vector<int> fami(n), foram(m);
+    vector<int> fami(n + 1), foram(m);
     map<int, int> graus, foramGraus;
 
-    for (int i = 0; i < n; i++) cin >> fami[i];
+    for (int i = 1; i <= n; i++) cin >> fami[i];
     for (int i = 0; i < m; i++) cin >> foram[i];
+    sort(foram.begin(), foram.end());
 
-    for(int i = 0; i < n; i++){
+    for(int i = 1; i <= n; i++){
 
         //Encontra o grau da pessoa i
         int grau = 0;
@@ -20,9 +21,17 @@ int main(){
             grau++;
             j = fami[j];
         }
-
         ++graus[grau];
+        if(binary_search(foram.begin(), foram.end(), i)){
+            ++foramGraus[grau];
+        }
     }
+    
+    cout << setprecision(2) << fixed;
 
+    for (auto x : graus){
+        cout << (((double)foramGraus[x.first]/x.second)*100.0) << " ";
+    }
+    
     return 0;
 }
