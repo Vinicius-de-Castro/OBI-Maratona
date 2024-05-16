0fee345c45a0608bf4c0402a3350602c7e03c7e2
Vinicius Castro <150952242+Vinicius-de-Castro@users.noreply.github.com>
2024-05-16 15:08:06 -0300

    Update l-quebra-cabeca.cpp

--- Semana 3/l-quebra-cabeca.cpp
+++ Semana 3/l-quebra-cabeca.cpp
@@ -3,11 +3,17 @@
 using namespace std;
 
 int main(){
-  int n;
+  int n, e, d;
+  char c;
   cin >> n;
+  map<int, pair<char, int>> mapa;
   while (n--){
-    int e, d;
-    
+    cin >> e >> c >> d;
+    mapa[e] = {c, d};
   }
+  for (auto x = mapa.find(0); x != mapa.end(); x = mapa.find(x->second.second)){
+    cout << x->second.first;
+  }
+  cout << endl;
   return 0;
 }
